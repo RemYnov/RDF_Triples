309eb0a083faab2729f5ed8b4af3c0f28007fc22
blremi <remi.tintane@bird-link.ch>
2023-04-18 16:16:08 +0200

    Debugging4 Spark tests

--- tests/test_spark_operations.py
+++ tests/test_spark_operations.py
@@ -62,5 +62,6 @@ def are_dataframes_equal(df1: DataFrame, df2: DataFrame) -> bool:
     # Vérifier si les différences entre les deux DataFrames sont vides
     df1_subtract_df2 = df1.subtract(df2)
     df2_subtract_df1 = df2.subtract(df1)
-
-    return True# df1_subtract_df2.count() == 0 and df2_subtract_df1.count() == 0
+    isEmpty = (df1_subtract_df2.count() == 0 and df2_subtract_df1.count() == 0)
+    print("Test results : ", isEmpty)
+    return isEmpty
