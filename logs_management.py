500fef4d8ea9ecdf20bc4440bfb50a75e4634c46
blremi <remi.tintane@bird-link.ch>
2023-04-20 09:52:44 +0200

    CI cache test with linter

--- logs_management.py
+++ logs_management.py
@@ -1,5 +1,6 @@
 import time
-from colorama import init, Fore, Style
+from colorama import Fore, Style
+
 
 class Logger:
     """
@@ -28,24 +29,34 @@ class Logger:
             self.colored_display(log_entry, level=self.customLog)
         if counter:
             self.increment_counter(counter)
+
     def colored_display(self, msg, level):
         if level == "normal": print(msg)
-        if level == "fancy": print(self.BLUE + msg + self.RESET)
-        elif level == "warning": print(self.YELLOW + msg + self.RESET)
-        elif level == "critical": print(self.RED + msg + self.RESET)
+        elif level == "fancy":
+            print(self.BLUE + msg + self.RESET)
+        elif level == "warning":
+            print(self.YELLOW + msg + self.RESET)
+        elif level == "critical":
+            print(self.RED + msg + self.RESET)
+
     def increment_counter(self, counter, increment=1):
         if counter not in self.counters:
             self.counters[counter] = 0
         self.counters[counter] += increment
+
     def get_counter(self, counter):
         return self.counters[counter]
+
     def start_timer(self, timer_name):
         self.timestamps[timer_name] = time.time()
+
     def stop_timer(self, timer_name):
         elapsed_time = time.time() - self.timestamps[timer_name]
         self.log(f"{timer_name} done in : {elapsed_time:.2f} seconds")
+
     def get_logs(self):
         return "\n".join(self.logs)
+
     def get_log_dict(self):
         return {
             "logs": self.logs,
