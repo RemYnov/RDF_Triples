85e863350bfbd9b3b68007eccaed32d6ca76ff1c
blremi <remi.tintane@bird-link.ch>
2023-04-20 17:56:12 +0200

    save

--- graph/neo4j_operations.py
+++ graph/neo4j_operations.py
@@ -1,2 +1,12 @@
-from logs_management import Logger
+import json
+from config import LOCAL_PREDICATES_TEMPLATE_PATH
 import pandas as pd
+
+def convert_template():
+    with open(LOCAL_PREDICATES_TEMPLATE_PATH, "r") as f:
+        predicates_json = json.load(f)
+
+    for key, value in predicates_json.items():
+        print(key, value)
+
+convert_template()
