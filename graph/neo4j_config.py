5822c5904f7726599de9b04d96ccee66b783f138
blremi <remi.tintane@bird-link.ch>
2023-05-15 22:11:00 +0200

    Starting to interract with Neo4j

--- graph/neo4j_config.py
+++ graph/neo4j_config.py
@@ -1,4 +1,4 @@
 NEO4J_URL = "bolt://localhost:7687"
 NEO4J_USER = "neo4j"
-NEO4J_PASSWORD = "your_password"
+NEO4J_PASSWORD = "neo4jthebest"
 CSV_PATH = "/path/to/your/csv/files"
--- graph/neo4j_manager.py
+++ graph/neo4j_manager.py
@@ -1,13 +1,18 @@
 from neo4j import GraphDatabase
 from neo4j_config import NEO4J_URL, NEO4J_USER, NEO4J_PASSWORD
+
 class GraphDbManager:
     def __init__(self):
         self.driver = GraphDatabase.driver(NEO4J_URL, auth=(NEO4J_USER, NEO4J_PASSWORD))
+        print('init ok')
 
     def close(self):
         self.driver.close()
 
-    def execute_query(self, query):
-        with self.driver.session() as session:
-            result = session.run(query)
-        return result
+    def execute_query(self, database, query, parameters=None):
+        if parameters is None:
+            parameters = {}
+
+        with self._driver.session(database=database) as session:
+            result = session.run(query, parameters)
+            return result.values()
